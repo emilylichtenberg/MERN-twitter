48cb061a672d014f9e4257b173fcf50fc6d75568
emilylichtenberg <erlichtenberg@gmail.com>
2022-03-13 20:48:25 -0400

    Add all front end tweet info

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 // import session from './session_reducer';
 import sessionReducer from './session_reducer';
 import errors from './errors_reducer'
+import TweetsReducer from './tweets_reducer';
 
 const RootReducer = combineReducers({
   session: sessionReducer,
-  errors
+  errors, 
+  tweets: TweetsReducer
 });
 
 export default RootReducer;
